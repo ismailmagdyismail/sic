1b5c5b619d0ae603304c35b4c6b5db89cc67063e
ismailmagdyismail <92678464+ismailmagdyismail@users.noreply.github.com>
2021-12-11 20:48:36 +0200

    Add files via upload

--- arduino_task2.ino
+++ arduino_task2.ino
@@ -1,32 +1,73 @@
-void setup() {
-pinMode(13,OUTPUT);
-pinMode(11,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(2,OUTPUT);
+int led1 =9;
+int led2=10;
+int ldr1=A0;
+int ldr2=A1;
+int ldr2value=0;
+int ldr1value=0;
+int buzzer=12;
+int people=0;
+int empty=0;
+
+void setup() 
+{
+  pinMode(led1,OUTPUT);
+  pinMode(ldr1,INPUT);
+  pinMode(ldr2,INPUT);
+  pinMode(buzzer,OUTPUT);
+  Serial.begin(9600);
+
 }
 
 void loop() 
 {
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(11,HIGH);
-  delay(1000);
-  digitalWrite(5,HIGH);
-  delay(1000);
-  digitalWrite(8,HIGH);
-  delay(1000);
-  digitalWrite(2,HIGH);
-  delay(2000);
-  digitalWrite(13,LOW);
-  digitalWrite(11,LOW);
-  digitalWrite(7,LOW);
-  digitalWrite(5,LOW);
-  digitalWrite(2,LOW);
-  digitalWrite(8,LOW);
-  delay(2000);
+ldr2value=analogRead(ldr2);
+ldr1value=analogRead(ldr1);
+Serial.print(people);
 
+ if(people<5)
+   {
+noTone(buzzer) ;   
+}
+if (ldr2value<=300)
+  {
+    digitalWrite(led2,HIGH);
+    delay(1500);
+    digitalWrite(led2,LOW);
+    if(people<5)
+    {
+      people=people+1;
+    }
+    else if(people=5)
+    {
+      if (ldr2value<=300)
+      {
+        tone(buzzer,1000);
+        delay(1000);
+        noTone(buzzer); 
+      }    
+   }
+  }
+ else
+    {
+    digitalWrite(led2,LOW);
+    }
+    if (ldr1value<=300)
+    {
+    digitalWrite(led1,HIGH);
+    delay(1500);
+    digitalWrite(led1,LOW);
+    if(people>0)
+    {
+      people=people-1;
+    }
+    else if(people<=0)
+    {
+      Serial.print(empty);
+    }
+  }
+  else
+    {
+    digitalWrite(led1,LOW);
+    }
+  
 }
